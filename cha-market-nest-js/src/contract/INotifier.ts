22042459f62cf470b3a790e46508c5cdda2f7341
JairGZZ <jairprox1234@gmail.com>
2025-12-08 23:56:53 -0500

    feat: update INotifier abstract class to include channel property and refined Send method signature

--- cha-market-nest-js/src/contract/INotifier.ts
+++ cha-market-nest-js/src/contract/INotifier.ts
@@ -1,8 +1,11 @@
-
 //segun lo que investigue, el patron strategy
-//  en nest js es mejor usando clases abstractas que 
+//  en nest js es mejor usando clases abstractas que
+
+import { CustomerDTO } from 'src/DTOs/CustomerDTO';
+import { RenderedContentDTO } from 'src/DTOs/RenderedContentDTO';
+
 // usando interfaces pero igual le ponder la I en el nombre jajaja
 export abstract class INotifier {
-    abstract Send();
+  abstract channel: string;
+  abstract Send(renderedDTO: RenderedContentDTO,CustomerDTO:CustomerDTO): Promise<void>;
 }
-
