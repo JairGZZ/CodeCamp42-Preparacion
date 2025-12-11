acdb12716c571ea3b84e98c052f23542418b9d77
JairGZZ <jairprox1234@gmail.com>
2025-12-10 19:16:16 -0500

    feat: add TemplateService for processing notification templates
    - Introduced TemplateService to handle the processing of notification data based on template keys.
    - Implemented methods for generating messages, including a specific DiscountTemplate for discount notifications.
    - Created RenderedContentDTO to structure the output of processed notifications.

---
+++ cha-market-nest-js/src/services/TemplateService.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { RenderedContentDTO } from 'src/DTOs/RenderedContentDTO';
+
+Injectable();
+export class TemplateService {
+  public ProcessNotificationData(
+    templateKey: string,
+    data: Record<string, string>,
+    channel: string,
+    eventId: string,
+  ): RenderedContentDTO {
+    const message = this.MessageAccordingTK(templateKey, data);
+
+    let dto = new RenderedContentDTO();
+    dto.body = message;
+    dto.channel = channel;
+    dto.eventid = eventId;
+    dto.subject = data['subject'];
+    return dto;
+  }
+  private MessageAccordingTK(
+    templateKey: string,
+    data: Record<string, string>,
+  ): string {
+    if (templateKey === 'descuento') {
+      return this.DiscountTemplate(templateKey, data);
+    }
+    return 'No template found';
+  }
+
+  private DiscountTemplate(
+    templateKey: string,
+    data: Record<string, string>,
+  ): string {
+    let message: string = '';
+    if (templateKey === 'descuento') {
+      message = `Hola ${data['name']} , accediste a un descuento 
+             del ${data['descuento']} 
+             en productos de ${data['categoria']}
+             Oferta valida hasta el ${data['fecha']}`;
+    }
+    return message;
+  }
+}
